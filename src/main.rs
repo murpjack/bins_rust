ebf8086a67f368b6bb548b0cbc3bbb241c9e24ea
jack.murphy <jack.murphy@featurespace.co.uk>
2023-02-16 21:53:47 +0000

    Refactor and tidy

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,81 @@
 use serde_derive::{Deserialize, Serialize};
-use serde_json;
 use std::time::SystemTime;
 
 fn main() {
-    match get_data() {
-        Ok(bin_day) => {
-            // TODO: Remove this '!'
-            if !is_bin_day_near(bin_day.date) {
-                let bins_str = bin_day
-                    .bins
-                    .iter()
-                    .map(BinType::to_icon)
-                    .map(String::from)
-                    .reduce(|cur: String, nxt| cur + &nxt)
-                    .unwrap();
-                println!("Bin day! {}", bins_str);
+    show_message()
+}
+
+fn show_message() -> () {
+    // TODO: Remove unwrap & handle errors
+    let bin_str = std::fs::read_to_string("src/bins.json").unwrap();
+    let bin_day = serde_json::from_str::<serde_json::Value>(&bin_str)
+        .unwrap()
+        .as_array()
+        .unwrap()
+        .iter()
+        .find_map(|raw_day| match raw_day["date"].as_str() {
+            Some(date_str) => {
+                let date = date_str.parse::<u128>().unwrap();
+                // TODO: Remove this '!' otherwise print first date in json file
+                if !is_bin_day_near(date) {
+                    return maybe_bin_day(raw_day);
+                } else {
+                    return None;
+                }
             }
+            _ => None,
+        });
+
+    match bin_day {
+        Some(today) => {
+            let icons = BinDay::bins_to_icons(&today);
+            println!("Bin day! {}", icons)
         }
-        Err(err) => panic!("Not today because {}", err),
+        _ => panic!("Not today"),
+    }
+}
+
+fn maybe_bin_day(ab: &serde_json::Value) -> Option<BinDay> {
+    match (ab["date"].as_str(), ab["bins"].as_array()) {
+        (Some(date_str), Some(raw_bins)) => Some(BinDay {
+            date: date_str.parse::<u128>().unwrap(),
+            bins: raw_bins
+                .iter()
+                .map(|raw_bin| BinType::from_str(raw_bin.as_str().unwrap()))
+                .collect::<Vec<BinType>>(),
+        }),
+        _ => None,
+    }
+}
+
+fn is_bin_day_near(next_date: u128) -> bool {
+    let one_day = 72000000;
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => {
+            // TODO: today_start should == today at 12am
+            // TODO: tomorrow_end should == tomorrow at 11.59pm
+            let now = n.as_millis();
+            let today_start = now - (one_day / 2);
+            let tomorrow_end = now + one_day;
+            next_date >= today_start && next_date < tomorrow_end
+        }
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct BinDay {
+    date: u128,
+    bins: Vec<BinType>,
+}
+
+impl BinDay {
+    fn bins_to_icons(&self) -> String {
+        self.bins
+            .iter()
+            .map(|bin_type| String::from(BinType::to_icon(bin_type)))
+            .reduce(|cur: String, nxt| cur + &nxt)
+            .unwrap()
     }
 }
 
@@ -46,59 +104,5 @@ impl BinType {
             "rubbish" => BinType::Landfill,
             _ => BinType::NoBin,
         }
-    }
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-struct BinDay {
-    date: u128,
-    bins: Vec<BinType>,
-}
-
-fn get_data() -> Result<BinDay, String> {
-    // TODO: Parse data here
-    // TODO: Remove unwrap & handle errors
-    let bin_str = std::fs::read_to_string("src/bins.json").unwrap();
-    let raw_bin_days = serde_json::from_str::<serde_json::Value>(&bin_str).unwrap();
-
-    let bin_days = raw_bin_days
-        .as_array()
-        .unwrap()
-        .iter()
-        .map(|raw_bin_day: &serde_json::Value| {
-            let raw_day: serde_json::Value = raw_bin_day.to_owned();
-            let not_raw = raw_day.as_object().unwrap().to_owned();
-            let dt: u128 = (not_raw["date"].as_str().unwrap()).parse().unwrap();
-            let bins: Vec<serde_json::Value> = raw_day["bins"].as_array().unwrap().to_owned();
-
-            return BinDay {
-                date: dt,
-                bins: bins
-                    .iter()
-                    .map(|x| return x.as_str().unwrap())
-                    .map(|bin_str| BinType::from_str(bin_str))
-                    .collect::<Vec<BinType>>(),
-            };
-        })
-        .collect::<Vec<BinDay>>();
-
-    // TODO: This is always the first value in the list
-    let firsty: BinDay = bin_days.first().cloned().unwrap();
-
-    return Result::Ok(firsty);
-}
-
-fn is_bin_day_near(bin_day: u128) -> bool {
-    let one_day = 72000000;
-    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => {
-            // TODO: today_start should == today at 12am
-            // TODO: tomorrow_end should == tomorrow at 11.59pm
-            let now = n.as_millis();
-            let today_start = now - (one_day / 2);
-            let tomorrow_end = now + one_day;
-            return bin_day >= today_start && bin_day < tomorrow_end;
-        }
-        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
 }
