cdd6b78f07f779e4ad6d96c457f7eff5603b1f43
jack.murphy <jack.murphy@featurespace.co.uk>
2023-03-21 10:36:13 +0000

    Fix second calculation in find_nearest bin method

--- src/bins.rs
+++ src/bins.rs
@@ -7,7 +7,6 @@ pub struct BinDay {
     bins: Vec<BinType>,
 }
 
-// TODO: Can this be removed?
 #[derive(Deserialize, Serialize)]
 struct RawBinDay {
     date: String,
@@ -18,18 +17,17 @@ impl BinDay {
     fn bins_to_icons(&self) -> String {
         self.bins
             .iter()
-            .map(|bin_type| String::from(BinType::to_icon(bin_type)))
-            .reduce(|cur: String, nxt| cur + &nxt)
-            .unwrap()
+            .map(BinType::to_icon)
+            .collect::<Vec<String>>()
+            .join("")
     }
 
     fn find_nearest(&self) -> Option<&Self> {
-        let day_secs = Duration::new(((1000 * 60) * 60) * 24, 0); // 86400000;
+        let day_secs = Duration::new((60 * 60) * 24, 0); // 86400000;
 
-        let now = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-            Ok(t) => t,
-            Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-        };
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .map_or_else(|_| panic!("SystemTime before UNIX EPOCH!"), identity);
 
         let next = self
             .date
@@ -45,11 +43,9 @@ impl BinDay {
             })
     }
 
-    fn read_list_of_dates() -> Vec<BinDay> {
-        let json_string_ref = match std::fs::read_to_string("./bins.json") {
-            Result::Ok(json_string) => json_string,
-            Result::Err(e) => panic!("{}", e.to_string()),
-        };
+    fn decode() -> Vec<BinDay> {
+        let json_string_ref = std::fs::read_to_string("./bins.json")
+            .map_or_else(|err| panic!("{}", err.to_string()), identity);
 
         match serde_json::from_str::<Vec<RawBinDay>>(&json_string_ref) {
             serde_json::Result::Ok(json_list) => json_list
@@ -59,16 +55,16 @@ impl BinDay {
                         .date
                         .parse::<u128>()
                         .map_or_else(|err| panic!("{}", err.to_string()), identity),
-                    bins: day.bins.iter().map(BinType::from_string).collect(),
+                    bins: day.bins.iter().map(BinType::decode).collect(),
                 })
-                .collect(),
+                .collect::<Vec<BinDay>>(),
 
             serde_json::Result::Err(err) => panic!("{}", err.to_string()),
         }
     }
 
     pub fn print() -> String {
-        BinDay::read_list_of_dates()
+        BinDay::decode()
             .iter()
             .find_map(BinDay::find_nearest)
             .map(BinDay::bins_to_icons)
@@ -84,7 +80,7 @@ enum BinType {
 }
 
 impl BinType {
-    fn from_string(bin_string: &String) -> Self {
+    fn decode(bin_string: &String) -> Self {
         match bin_string.as_str() {
             "recycling" => BinType::Recyclable,
             "food and garden waste" => BinType::Biodegradable,
@@ -93,12 +89,13 @@ impl BinType {
         }
     }
 
-    fn to_icon(&self) -> &str {
-        match self {
+    fn to_icon(&self) -> String {
+        let icon = match self {
             BinType::Recyclable => "♻️ ",
             BinType::Biodegradable => "💩",
             BinType::Landfill => "🗑",
             BinType::NoBin => "",
-        }
+        };
+        String::from(icon)
     }
 }
